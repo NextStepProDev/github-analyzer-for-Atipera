1dbd8d45c1117d73a46c837c9c0244bb2aa0ff08
Mateusz Nawratek <mateusz_nawratek@wp.pl>
2025-07-21 21:54:52 +0200

    test: simplify integration test setup and add custom @IntegrationTest annotation

--- src/test/java/com/example/githubAnalyzer/controller/GitHubControllerIntegrationTest.java
+++ src/test/java/com/example/githubAnalyzer/controller/GitHubControllerIntegrationTest.java
@@ -1,10 +1,10 @@
 package com.example.githubAnalyzer.controller;
 
+import com.example.githubAnalyzer.config.IntegrationTest;
 import com.example.githubAnalyzer.dto.RepositoryDTO;
 import com.example.githubAnalyzer.exception.GlobalExceptionHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@IntegrationTest
 public class GitHubControllerIntegrationTest {
 
     @LocalServerPort
